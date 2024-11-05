6dd5c634fb39e51f936f2e4523c586cd466fc3fa
MuhammadNur <delusionboys974@gmail.com>
2024-11-05 09:55:30 +0700

    feat(button): to add product and show form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import ProductLists from './components/ProductLists'
+import ProductCreate from './components/ProductCreate'
 
 
 const App = () => {
  return (
   <>
     <div className='app-title'>List Of Product</div>
+    <ProductCreate />
     <ProductLists />
   </>
  )
---
+++ src/components/ProductCreate.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+
+const ProductCreate = ({onCreateProduct}) => {
+    const initialState = {
+        'name': '',
+        'description': '',
+        'imageURL': ''
+    }
+    const [fromAdd, setFormAdd] = useState(initialState)
+    const { name, description, imageURL } = fromAdd
+    const [showForm, setShowForm] = useState(false)
+    const handleChange = (e) => {
+        setFormAdd({ ...fromAdd, [e.target.name]: e.target.value })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        onCreateProduct(fromAdd)
+        setFormAdd(initialState)
+    }
+    const handleToggleClick = () => {
+        setShowForm(!showForm)
+    }
+  return (
+    <div className='product-create'>
+        <div className='toggle-add'>
+            <button onClick={handleToggleClick} className='add-toggle edit-input-submit'>
+                {showForm ? 'Close Form' : 'Add Product'}
+            </button>
+        </div>
+        {showForm && (
+            <form className='form' onSubmit={handleSubmit}>
+                <div className='form-add-title'>Form Create Product</div>
+                <div className='form-group'>
+                    <input type="text" className='add-input-text' 
+                        placeholder='Name of product'
+                        name='name'
+                        value={name}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='form-group'>
+                    <input type="text" className='add-input-text' 
+                        placeholder='Description of product'
+                        name='description'
+                        value={description}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='form-group'>
+                    <input type="text" className='add-input-text' 
+                        placeholder='Image URL'
+                        name='imageURL'
+                        value={imageURL}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='add-input-submit'>
+                    <button className='add'>Add</button>
+                </div>
+            </form>
+        )}
+    </div>
+  )
+}
+
+export default ProductCreate
